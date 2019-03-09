f837b6211524670ca0991124b8bf3e4d3fa11b59
Rokas Gulbinas <rkgulbinas@gmail.com>
2019-03-09 23:57:38 +0200

    Add '/' command + Enter combination, fix '/' commands not working from newline, address some code style issues.

--- ToNote/Controls/ExtendedRichTextBox.cs
+++ ToNote/Controls/ExtendedRichTextBox.cs
@@ -26,51 +26,50 @@
             this.TextChanged += (s, e) =>
             {
                 //Prevents start of tracking while initializing.
-                if (this.IsLoaded)
+                if (!this.IsLoaded) return;
+
+                // -3, because text ending has \r\n chars
+                var offset = range.Text.Length - 3;
+                var secondToLastChar = offset - 1 >= 0 ? range.Text[offset - 1] : ' ';
+                var lastChar = offset >= 0 ? range.Text[offset] : ' ';
+
+                if (_tracking)
                 {
-                    // -3, because text ending has \r\n chars
-                    var offset = range.Text.Length - 3;
-                    var secondToLastChar = offset - 1 >= 0 ? range.Text[offset - 1] : ' ';
-                    var lastChar = offset >= 0 ? range.Text[offset] : ' ';
-
-                    if (_tracking)
+                    //Stops tracking after 10 text changes without a '/' being the last character.
+                    if (_trackingCounter++ > 10)
                     {
-                        //Stops tracking after 10 text changes without a '/' being the last character.
-                        if (_trackingCounter++ > 10)
-                        {
-                            _tracking = false;
-                        }
-
-                        //If text is deleted beyond '/', stops tracking
-                        if (offset - _slashIndex < 0)
-                        {
-                            _tracking = false;
-                            return;
-                        }
-
-                        var keyword = range.Text.Substring(_slashIndex + 1, offset - _slashIndex);
-
-                        foreach (var keywordaction in _trackedKeywords)
-                        {
-                            if (keyword != keywordaction.Keyword + ' ') return;
-
-                            _tracking = false;
-
-                            //Trims the '/' + keyword from the end of the text
-                            range.Text = range.Text.Remove(offset - keyword.Length, keyword.Length + 1);
-
-                            keywordaction.Action.Invoke();
-
-                            return;
-                        }
+                        _tracking = false;
                     }
 
-                    if (lastChar == '/' && secondToLastChar == ' ')
+                    //If text is deleted beyond '/', stops tracking
+                    if (offset - _slashIndex < 0)
                     {
-                        _tracking = true;
-                        _slashIndex = offset;
-                        _trackingCounter = 0;
+                        _tracking = false;
+                        return;
                     }
+
+                    var keyword = range.Text.Substring(_slashIndex + 1, offset - _slashIndex);
+
+                    foreach (var keywordAction in _trackedKeywords)
+                    {
+                        if (keyword != keywordAction.Keyword + ' ' && keyword != keywordAction.Keyword + '\r' + '\n') continue;
+
+                        _tracking = false;
+
+                        //Trims the '/' + keyword from the end of the text
+                        range.Text = range.Text.Remove(offset - keyword.Length, keyword.Length + 1);
+
+                        keywordAction.Action.Invoke();
+
+                        return;
+                    }
+                }
+
+                if (lastChar == '/' && secondToLastChar == ' ' || secondToLastChar == '\n')
+                {
+                    _tracking = true;
+                    _slashIndex = offset;
+                    _trackingCounter = 0;
                 }
             };
         }
@@ -102,8 +101,8 @@
         public void TrackKeyword(string keyword, Action action)
         {
             var keywordaction = new KeywordAction() { Keyword = keyword, Action = action };
-            if (!_trackedKeywords.Contains(keywordaction))
-                _trackedKeywords.Add(keywordaction);
+
+            _trackedKeywords.Add(keywordaction);
         }
 
         private struct KeywordAction
