a7dddd875b19dfae24995c5413b1062e30de0677
Audrius Liuberskis <audriusliub@gmail.com>
2019-04-15 17:51:16 +0300

    Added base stuff for testing project. Added a failing test to test pipeline

---
+++ ToNoteTests/Converters/BoolToResizeModeConverterTests.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+using System;
+using ToNote.Converters;
+using Xunit;
+using System.Windows;
+
+namespace ToNoteTests.Converters
+{
+    public class BoolToResizeModeConverterTests
+    {
+        [Fact]
+        public void Convert_WhenValueArgumentIsNotBool_ShouldThrow()
+        {
+            // Arrange
+            var converter = new BoolToResizeModeConverter();
+
+            // Act, Assert
+            Should.Throw<ArgumentException>(() => converter.Convert("random" as object, null, null, null))
+                 .Message.ShouldBe("Wrong data type. Expected a boolean");
+        }
+
+        [Theory]
+        [InlineData(true, ResizeMode.CanResize)]
+        [InlineData(false, ResizeMode.NoResize)]
+        public void Convert_WhenValueArgumentIsBool_ShouldReturnCorrespondingResizeMode(bool value, ResizeMode mode)
+        {
+            // Arrange
+            var converter = new BoolToResizeModeConverter();
+
+            // Act
+            var result = converter.Convert(value, null, null, null);
+
+            // Assert
+            ((ResizeMode)result).ShouldBe(mode);
+        }
+
+        [Fact]
+        public void ConvertBack_WhenCalled_ShouldThrow()
+        {
+            // Arrange
+            var converter = new BoolToResizeModeConverter();
+
+            //Act, Assert
+            // Intentionally failing test to test pipeline
+            Should.Throw<ArgumentNullException>(() => converter.ConvertBack(null, null, null, null));
+        }
+    }
+}
