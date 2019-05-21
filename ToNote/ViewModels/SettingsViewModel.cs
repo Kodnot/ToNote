31b050d3eed7eac23bd56879c820a24a67bc33b8
Hofkee <dkuprys@gmail.com>
2019-05-21 23:20:41 +0300

    Einde

--- ToNote/ViewModels/SettingsViewModel.cs
+++ ToNote/ViewModels/SettingsViewModel.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Data;
-using System.Windows.Input;
-using ToNote.Logic;
-using ToNote.Models;
-using ToNote.ViewModels;
+﻿using System.Windows.Input;
 
 namespace ToNote.ViewModels
 {
@@ -18,9 +7,5 @@ namespace ToNote.ViewModels
         public ICommand ImportCommand { get; set;}
         public ICommand ExportCommand { get; set;}
 
-        public SettingsViewModel()
-        {
-
-        }
     }
 }
