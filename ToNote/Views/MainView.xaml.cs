5d625a79549665c1f9fb66c12e24b73452c37807
CasuaIAz <arijustreinys@gmail.com>
2019-05-27 16:52:19 +0300

    Fixed: Fullscreen slightly crops the borders of the window.

--- ToNote/Views/MainView.xaml.cs
+++ ToNote/Views/MainView.xaml.cs
@@ -17,7 +17,12 @@
             InitializeComponent();
 
             //Prevents the maximize button from covering taskbar. Doesn't work well if multiple monitors of different resolutions are used.
-            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            var state = WindowState.Normal;
+            var currentHeight = this.Height;
+            var currentWidth = this.Width;
+            var desktopWorkingArea = SystemParameters.WorkArea;
+            this.MaxHeight = desktopWorkingArea.Height;
+            this.MaxWidth = desktopWorkingArea.Width;
 
             btn_MinimizeWindow.Click += (s, e) =>
             {
@@ -27,14 +32,24 @@
 
             btn_MaximizeWindow.Click += (s, e) =>
             {
-                if (this.WindowState != WindowState.Maximized)
+                if (state != WindowState.Maximized)
                 {
-                    this.WindowState = WindowState.Maximized;
+                    state = WindowState.Maximized;
+                    this.Width = desktopWorkingArea.Width;
+                    this.Height = desktopWorkingArea.Height;
+                    this.Left = desktopWorkingArea.Left;
+                    this.Top = desktopWorkingArea.Top;
                     return;
                 }
 
-                if (this.WindowState == WindowState.Maximized)
-                    this.WindowState = WindowState.Normal;
+                if (state == WindowState.Maximized)
+                {
+                    state = WindowState.Normal;
+                    this.Width = currentWidth;
+                    this.Height = currentHeight;
+                    this.Left = this.MaxWidth / 2 - currentWidth / 2;
+                    this.Top = this.MaxHeight / 2 - currentHeight / 2;
+                }
             };
 
             btn_CloseWindow.Click += (s, e) =>
