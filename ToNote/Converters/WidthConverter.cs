bf04657c490c390a70693aa57449cbbe0d492932
CasuaIAz <arijustreinys@gmail.com>
2019-05-28 12:20:23 +0300

    EDIT: Created a seperate file for WidthConverter...

--- ToNote/Converters/WidthConverter.cs
+++ ToNote/Converters/WidthConverter.cs
@@ -4,19 +4,12 @@
     using System.Windows;
     using System.Windows.Data;
 
-    public class BoolToResizeModeConverter : IValueConverter
+    public class WidthConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (value is bool resizeable)
-            {
-                if (resizeable)
-                    return ResizeMode.CanResize;
-                else
-                    return ResizeMode.NoResize;
-            }
-            else
-                throw new ArgumentException("Wrong data type. Expected a boolean");
+            if (value == null) return 0;
+            return (double)value - 60;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
